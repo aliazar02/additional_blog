a88e6d2c356e15f9f51fed58585e908ddf0f1074
alex <aliazarwoldemariam@gmail.com>
2024-11-10 17:19:29 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function drop_downs() {
+    const menuBar = document.querySelector('.drop_down_menu')
+    menuBar.style.transform = 'translateY(0px)' 
+}
+function drop_ups() {
+    const menuBar = document.querySelector('.drop_down_menu')
+    menuBar.style.transform = 'translateY(-500px)' 
+}
